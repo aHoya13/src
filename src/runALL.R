c5f120a08ffabc6142f30134dc6b11ae7b300fb4
aHoya13 <annmarie.jacoby@gmail.com>
2018-12-03 14:31:18 -0500

    Put these scripts in a controller script, e.g. source(readData.R), source(plotData.R) etc.

---
+++ src/runALL.R
@@ -0,0 +1,6 @@
+#put these scripts in a controller script, e.g. source(readData.R), source(plotData.R) etc.
+source("src/readData.R")
+source("src/wrangleData.R")
+source("src/plotData.R")
+source("src/calcd2ship.R")
+source("src/runRegression.R")
