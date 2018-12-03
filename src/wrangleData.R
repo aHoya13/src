54e570e1ff52cb2b4720f31f50031d4ec8be93c3
aHoya13 <annmarie.jacoby@gmail.com>
2018-12-03 14:13:09 -0500

    "Selected the start of the CEE and combine the data frame"

--- src/wrangleData.R
+++ src/wrangleData.R
@@ -1,1 +1,4 @@
 #Lorem ipsum dolor amet photo booth DIY affogato yuccie man bun seitan glossier.≈
+# Minimal Wrangling of the data
+gmpts <- bind_rows(gm182, gm182UP)
+colnames(gmpts) <- c('trackNum', 'time', 'longitude', 'latitude', 'status')
